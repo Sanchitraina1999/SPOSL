276293ffe936d9db5196d6f98df8829b49bfb72e
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-02-11 08:55:48 +0530

    update: master

--- schedulingAlgorithms/FCFS.java
+++ schedulingAlgorithms/FCFS.java
@@ -40,6 +40,7 @@ public class FCFS {
         System.out.println("Average Waiting Time = " + avgWT);
         System.out.println("Average TAT = " + avgTAT);
 
+        //GANTT CHART
         int i, j;
         System.out.print(" ");
         for (i = 0; i < numProcess; i++) {
