58d790a14d1691541431c76bb28aa6fbd7405a8b
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-02-11 09:09:08 +0530

    update: master

--- schedulingAlgorithms/PriorityP.java
+++ schedulingAlgorithms/PriorityP.java
@@ -1,45 +1,40 @@
-import java.util.*; 
+import java.util.*;
+
 public class PriorityP {
-class Process { 
-    int at, bt, pri, pno; 
-    Process(int pno, int at, int bt, int pri) 
-    { 
-        this.pno = pno; 
-        this.pri = pri; 
-        this.at = at; 
-        this.bt = bt; 
-    } 
-} 
-  
-/// Gantt chart structure 
-class GChart { 
-    // process number, start time, complete time, 
-    // turn around time, waiting time 
-    int pno, stime, ctime, wtime, ttime; 
-} 
-  
-// user define comparative method (first arrival first serve, 
-// if arrival time same then heigh priority first) 
-class MyComparator implements Comparator { 
-  
-    public int compare(Object o1, Object o2) 
-    { 
-  
-        Process p1 = (Process)o1; 
-        Process p2 = (Process)o2; 
-        if (p1.at < p2.at) 
-            return (-1); 
-  
-        else if (p1.at == p2.at && p1.pri > p2.pri) 
-            return (-1); 
-  
-        else
-            return (1); 
-    } 
-} 
-  
-  
-// class to find Gantt chart 
+    class Proces {
+        int at, bt, pri, pno;
+        Proces(int pno, int at, int bt, int pri) {
+            this.pno = pno;
+            this.pri = pri;
+            this.at = at;
+            this.bt = bt;
+        }
+    }
+
+    class GChart {
+        int pno, stime, ctime, wtime, ttime;
+    }
+
+    // user define comparative method (first arrival first serve,
+    // if arrival time same then heigh priority first)
+    class MyComparator implements Comparator {
+
+        public int compare(Object o1, Object o2) {
+
+            Proces p1 = (Proces) o1;
+            Proces p2 = (Proces) o2;
+            if (p1.at < p2.at)
+                return (-1);
+
+            else if (p1.at == p2.at && p1.pri > p2.pri)
+                return (-1);
+
+            else
+                return (1);
+        }
+    }
+
+    // class to find Gantt chart
 class FindGantChart { 
     void findGc(LinkedList queue) 
     { 
@@ -54,21 +49,21 @@ class FindGantChart {
         // link list for store processes data 
         LinkedList result = new LinkedList(); 
   
-        // process in ready queue from new state queue 
+        // Proces in ready queue from new state queue 
         while (queue.size() > 0) 
-            prique.add((Process)queue.removeFirst()); 
+            prique.add((Proces)queue.removeFirst()); 
   
         Iterator it = prique.iterator(); 
   
-        // time set to according to first process 
-        time = ((Process)prique.first()).at; 
+        // time set to according to first Proces 
+        time = ((Proces)prique.first()).at; 
   
-        // scheduling process 
+        // scheduling Proces 
         while (it.hasNext()) { 
   
             // dispatcher dispatch the 
-            // process ready to running state 
-            Process obj = (Process)it.next(); 
+            // Proces ready to running state 
+            Proces obj = (Proces)it.next(); 
   
             GChart gc1 = new GChart(); 
             gc1.pno = obj.pno; 
@@ -78,11 +73,11 @@ class FindGantChart {
             gc1.ttime = gc1.ctime - obj.at; 
             gc1.wtime = gc1.ttime - obj.bt; 
   
-            /// store the exxtreted process 
+            /// store the exxtreted Proces 
             result.add(gc1); 
         } 
   
         // create object of output class and call method 
         new ResultOutput(result); 
     } 
-} 
+}
