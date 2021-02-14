3341310660f39236904b79b5ac46be97c1913981
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-02-14 16:27:10 +0530

    update: master

--- schedulingAlgorithms/RoundRobin.java
+++ schedulingAlgorithms/RoundRobin.java
@@ -99,3 +99,42 @@ public class RoundRobin {
     }
 
 }
+
+/*
+    imsotired@imsotired-Vostro-3578:~/Desktop/TE/SPOSL$  /usr/bin/env /usr/lib/jvm/java-11-openjdk-amd64/bin/java -Dfile.encoding=UTF-8 -cp /home/imsotired/.config/Code/User/workspaceStorage/557c4fc057c4787074b405cb8c60a66e/redhat.java/jdt_ws/SPOSL_ad48c254/bin RoundRobin 
+Enter Number of Processes:
+5
+P(1):Enter Arrival time & Burst time
+2 6
+P(2):Enter Arrival time & Burst time
+5 2
+P(3):Enter Arrival time & Burst time
+1 8
+P(4):Enter Arrival time & Burst time
+0 3
+P(5):Enter Arrival time & Burst time
+4 4
+Enter Quantum Time: 
+2
+
+
+PRNo    BT      AT      CT      TAT     WT      PR
+============================================================================================
+0 TIME 2
+1 TIME 4
+2 TIME 6
+3 TIME 8
+4 TIME 10
+P2      2       5       10      5       3       0
+0 TIME 11
+P4      3       0       11      11      8       0
+1 TIME 13
+2 TIME 15
+3 TIME 17
+P5      4       4       17      13      9       0
+1 TIME 19
+2 TIME 21
+P1      6       2       21      19      13      0
+1 TIME 23
+P3      8       1       23      22      14      0
+*/
