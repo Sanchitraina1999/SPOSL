8d2d5e4f78511fac44a09c9e9696e98c10a4f5c6
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-02-22 09:10:32 +0530

    update: master

--- schedulingAlgorithms/PriorityNP.java
+++ schedulingAlgorithms/PriorityNP.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class PriorityNP {
 	private Scanner sc;
 
-	public void execute() {
+	public static void main(String[] args) {
 		sc = new Scanner(System.in);
 
 		System.out.println("Enter Number of Processes:");
